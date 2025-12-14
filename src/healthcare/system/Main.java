01a74ce7b1d2bb3ebd4f42d8aa9c4af176ea415a
Hasan <23021813@herts.ac.uk>
2025-12-14 21:44:05 +0000

    feat: Initial project setup with MVC structure

---
+++ src/healthcare/system/Main.java
@@ -0,0 +1,8 @@
+package healthcare.system;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Healthcare Management System - Week 1");
+        System.out.println("Student: Hasan (23021813)");
+    }
+}
